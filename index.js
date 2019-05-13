ba6cf1ec8198d139b4b4562e1682ca84a278aee8
James M. Greene <jamesmgreene@github.com>
2019-05-13 18:09:34 -0500

    Comment out almost all the code

--- index.js
+++ index.js
@@ -19,48 +19,50 @@ const requiredNonSecretEnvVars = [
 
 const tools = new Toolkit({
   // If the event received is not included, Toolkit will exit neutrally
-  event: ['issues.opened'],
+  event: ['issues.opened'] //,
   // If the following secrets are not present, Toolkit will exit with a failure
-  secrets: ['GOOGLE_SHEETS_API_KEY', ...requiredNonSecretEnvVars],
+  //secrets: ['GOOGLE_SHEETS_API_KEY', ...requiredNonSecretEnvVars],
 })
 
 tools.log.info('Welcome!')
+tools.exit.success('We did it!')
+
 
 // Wrap into an `async` function so we can using `await`
-async function main() {
-  const {
-    GOOGLE_SHEETS_API_KEY,
-    SPREADSHEET_ID,
-    SHEET_NAME,
-    DATE_ROW,
-    LOGIN_COL,
-  } = process.env
+// async function main() {
+//   const {
+//     GOOGLE_SHEETS_API_KEY,
+//     SPREADSHEET_ID,
+//     SHEET_NAME,
+//     DATE_ROW,
+//     LOGIN_COL,
+//   } = process.env
 
-  const sheets = google.sheets({
-    version: 'v4',
-    auth: GOOGLE_SHEETS_API_KEY,
-  })
+//   const sheets = google.sheets({
+//     version: 'v4',
+//     auth: GOOGLE_SHEETS_API_KEY,
+//   })
 
-  const [dateRowRes, loginColRes] = await Promise.all(
-    sheets.spreadsheets.values.get({
-      spreadsheetId: SPREADSHEET_ID,
-      range: `'${SHEET_NAME}'!${DATE_ROW}:${DATE_ROW}`,
-    }),
-    sheets.spreadsheets.values.get({
-      spreadsheetId: SPREADSHEET_ID,
-      range: `'${SHEET_NAME}'!${LOGIN_COL}:${LOGIN_COL}`,
-    })
-  )
+//   const [dateRowRes, loginColRes] = await Promise.all(
+//     sheets.spreadsheets.values.get({
+//       spreadsheetId: SPREADSHEET_ID,
+//       range: `'${SHEET_NAME}'!${DATE_ROW}:${DATE_ROW}`,
+//     }),
+//     sheets.spreadsheets.values.get({
+//       spreadsheetId: SPREADSHEET_ID,
+//       range: `'${SHEET_NAME}'!${LOGIN_COL}:${LOGIN_COL}`,
+//     })
+//   )
 
-  tools.log.info('Payload:')
-  tools.log.info(JSON.stringify(tools.context.payload))
-  tools.log.info('Date row res:')
-  tools.log.info(JSON.stringify(dateRowRes))
-  tools.log.info('Login column res:')
-  tools.log.info(JSON.stringify(loginColRes))
+//   tools.log.info('Payload:')
+//   tools.log.info(JSON.stringify(tools.context.payload))
+//   tools.log.info('Date row res:')
+//   tools.log.info(JSON.stringify(dateRowRes))
+//   tools.log.info('Login column res:')
+//   tools.log.info(JSON.stringify(loginColRes))
 
-  tools.exit.success('We did it!')
-}
+//   tools.exit.success('We did it!')
+// }
 
-// Run the main function
-main().catch(err => tools.exit.failure(err.message))
+// // Run the main function
+// main().catch(err => tools.exit.failure(err.message))
