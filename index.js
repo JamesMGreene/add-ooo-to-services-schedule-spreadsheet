56c72654109cbb9d7d4ed9b8a1abf9befbb66b82
James M. Greene <jamesmgreene@github.com>
2019-05-13 17:44:27 -0500

    Initial Google API testing

--- index.js
+++ index.js
@@ -1,6 +1,66 @@
+// Userland modules
 const { Toolkit } = require('actions-toolkit')
+const { google } = require('googleapis')
 
-// Run your GitHub Action!
-Toolkit.run(async tools => {
-  tools.exit.success('We did it!')
-})
+// Test spreadsheet = 1GoMPfZBppYwKjdu_GR5ZCGhNBgocPfhvvSq4W6S3c2I
+// Real spreadsheet = 1jaLzkVG3BmV2fPjcKoWPeq6kRXB9Lcpqk28r-DcPeZo
+// process.env.SPREADSHEET_ID = '1GoMPfZBppYwKjdu_GR5ZCGhNBgocPfhvvSq4W6S3c2I'
+
+// process.env.SHEET_NAME = '2019-neworg'
+// process.env.DATE_ROW = '1'
+// process.env.LOGIN_COL = 'B'
+
+const requiredNonSecretEnvVars = [
+  'SPREADSHEET_ID',
+  'SHEET_NAME',
+  'DATE_ROW',
+  'LOGIN_COL'
+]
+
+// Wrap with an `async` IIFE so we can using `await`
+(async () => {
+  const tools = new Toolkit({
+    // If the event received is not included, Toolkit will exit neutrally
+    event: ['issues.opened'],
+    // If the following secrets are not present, Toolkit will exit with a failure
+    secrets: ['GOOGLE_SHEETS_API_KEY', ...requiredNonSecretEnvVars]
+  })
+
+  const {
+    GOOGLE_SHEETS_API_KEY,
+    SPREADSHEET_ID,
+    SHEET_NAME,
+    DATE_ROW,
+    LOGIN_COL
+  } = process.env
+
+  try {
+    const sheets = google.sheets({
+      version: 'v4',
+      auth: GOOGLE_SHEETS_API_KEY
+    })
+
+    const [dateRowRes, loginColRes] = await Promise.all(
+      sheets.spreadsheets.values.get({
+        spreadsheetId: SPREADSHEET_ID,
+        range: `'${SHEET_NAME}'!${DATE_ROW}:${DATE_ROW}`
+      }),
+      sheets.spreadsheets.values.get({
+        spreadsheetId: SPREADSHEET_ID,
+        range: `'${SHEET_NAME}'!${LOGIN_COL}:${LOGIN_COL}`
+      })
+    )
+
+    tools.log.info('Payload:')
+    tools.log.info(JSON.stringify(tools.context.payload))
+    tools.log.info('Date row res:')
+    tools.log.info(JSON.stringify(dateRowRes))
+    tools.log.info('Login column res:')
+    tools.log.info(JSON.stringify(loginColRes))
+
+    tools.exit.success('We did it!')
+  }
+  catch (err) {
+    tools.exit.failure(err.message)
+  }
+})()
