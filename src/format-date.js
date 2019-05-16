ec2050b28dda86f664a4ed89a423be1c8c27c69a
James M. Greene <jamesmgreene@github.com>
2019-05-16 15:25:11 -0500

    Simplify 'format-date' implementation

--- src/format-date.js
+++ src/format-date.js
@@ -1,21 +1,15 @@
-function leftPadWithZeroes(str, len) {
-  str = '' + str
-  len = len - str.length
-  while (len > 0) {
-    str = '0' + str
-    len--
-  }
-  return str
+function isRealDate(date) {
+  return Object.prototype.toString.call(date) === '[object Date]'
+}
+
+function pad(num) {
+  return (num < 10 ? '0' : '') + num
 }
 
 function formatDate(date) {
   if (!date) return null
-  if (Object.prototype.toString.call(date) === '[object Date]')
-    return date.toISOString().slice(0, 10)
-  return `${date.year}-${leftPadWithZeroes(date.month, 2)}-${leftPadWithZeroes(
-    date.day,
-    2
-  )}`
+  if (isRealDate(date)) return date.toISOString().slice(0, 10)
+  return `${date.year}-${pad(date.month)}-${pad(date.day)}`
 }
 
 module.exports = formatDate
